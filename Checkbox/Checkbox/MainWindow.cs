00ab57db2fc5d3835e80447bbc97311764e4009e
Jose Gonzalez <jozho.g@gmail.com>
2019-09-27 11:01:45 -0300

    Oli

---
+++ Checkbox/Checkbox/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+using System.Text;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButton10Clicked(object sender, EventArgs e)
+    {
+        Title = "";
+        if (checkbutton2.Active == true)
+        {
+            Title = Title + "(Frances)";
+        }
+
+    }
+}
