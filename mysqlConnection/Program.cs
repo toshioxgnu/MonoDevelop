a172a61cb52f3ad7f26d8128ac8f59f4007be12a
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-11-14 18:57:12 -0300

    Empezando con mysql

---
+++ mysqlConnection/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using MySql.Data.MySqlClient;
+
+namespace mysqlConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String connectionString = null;
+            MySqlConnection connection1;
+            connectionString = "server=localhost;database=Alumnos;uid=root;pwd=root;";
+            connection1 = new MySqlConnection(connectionString);
+             try
+            {
+                Console.WriteLine("Connection stablished");
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine("Can not open connection ! ");
+            }
+        }
+    }
+}
